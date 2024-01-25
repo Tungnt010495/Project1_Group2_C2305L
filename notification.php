f5614cf503332d358b7d358603896edadea0cd72
nhatpm00 <142024477+nhatpm00@users.noreply.github.com>
2024-01-25 23:12:43 +0700

    Add files via upload

---
+++ notification.php
@@ -0,0 +1,16 @@
+
+<br><br><br>
+<div class="container mt-3">
+    <div class="col-12 text-center">
+    <h2 style="color:#0071e3">Order Success</h2>
+    </div>
+    <div class="col-12">
+    <a href="?page_name=home" class="cart-next-index">
+        &larr; Continue shopping
+    </a>
+
+    </div>
+    
+
+
+</div>
